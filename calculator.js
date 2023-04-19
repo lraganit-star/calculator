0229b296e4a7c23171b38ac0867b8828c7e6c550
Leslie Raganit <niki@Leslies-MacBook-Pro.local>
2023-04-18 17:21:18 -0700

    add operations

--- calculator.js
+++ calculator.js
@@ -1,8 +1,12 @@
 var display = document.getElementById("display");
 const numberButtons = document.getElementsByClassName("number");
+const operationButtons = document.getElementsByClassName("operator")
 const clearButton = document.getElementById("clear")
 
 var inputVal = "";
+var num1 = "";
+var num2 = "";
+var operation = "";
 
 for (const numButton of numberButtons) {
     numButton.addEventListener("click", function() {
@@ -12,14 +16,23 @@ for (const numButton of numberButtons) {
     })
 }
 
+for (const opButton of operationButtons) {
+    opButton.addEventListener("click", function(){
+
+        operation == opButton.innerHTML;
+        display.innerHTML += opButton.innerHTML;
+        inputVal += opButton
+    })
+    console.log(opButton.innerHTML)
+}
+
 
 clearButton.addEventListener("click", function() {
     display.innerHTML = ""
+    inputVal == ""
 })
 
-var num1 = "";
-var num2 = "";
-var operation = "";
+
 
 function addition(num1, num2) {
     return num1 + num2
@@ -38,6 +51,7 @@ function divide(num1, num2) {
 }
 
 function operate(num1, operator, num2) {
+    display.innerHTML += operator;
     switch (operator) {
         case "+": 
             return addition(num1, num2);
@@ -49,3 +63,5 @@ function operate(num1, operator, num2) {
             return divide(num1, num2);
     }
 }
+
+console.log(operation)
